d78d2ff0bac92393e878829c2c62b63ff0d7c5c8
Habibur Rahman <habib.swi16@gmail.com>
2023-08-11 23:11:18 +0600

    selectElements

---
+++ selecElements/main.js
@@ -0,0 +1,22 @@
+// dom manipulation
+
+// getElementById
+
+const title = document.getElementById("main-heading");
+console.log(title);
+
+// getElementsByClassName
+
+const listItem = document.getElementsByClassName("list-items");
+
+console.log(listItem);
+
+// getElementsByTagName
+const listTagItem = document.getElementsByTagName("li");
+
+console.log(listTagItem);
+// querySelector
+const container=document.querySelector("div")
+console.log(container);
+
+// querySelectorAll

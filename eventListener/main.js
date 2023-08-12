33b60a42d14ac684524d0c367165cc9fb1e93c38
Habibur Rahman <habib.swi16@gmail.com>
2023-08-12 14:46:39 +0600

    eventlistener

---
+++ eventListener/main.js
@@ -0,0 +1,21 @@
+// Event Listener
+
+// element.addEventListener("click",function)
+
+const buttonTwo = document.querySelector(".btn-2");
+
+function alertBtn() {
+  alert("hello");
+}
+
+buttonTwo.addEventListener("click", alertBtn);
+
+// Mouseover
+
+const newBackgroundColor = document.querySelector(".box-3");
+
+function changeBgColor() {
+  newBackgroundColor.style.backgroundColor = "blue";
+}
+
+newBackgroundColor.addEventListener("mouseover", changeBgColor);
